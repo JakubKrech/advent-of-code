155cb19d46df01bffdad5804c9751b4001d7e265
Jakub <kuba.kre@o2.pl>
2023-12-12 22:28:27 +0000

    Day_12 solution

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 - **Day 9: [Mirage Maintenance](advent-of-code-2023/advent-of-code-2023/Solutions/Day_09.cs)**
 - **Day 10: [Pipe Maze](advent-of-code-2023/advent-of-code-2023/Solutions/Day_10.cs)**
 - **Day 11: [Cosmic Expansion](advent-of-code-2023/advent-of-code-2023/Solutions/Day_11.cs)**
+- **Day 12: [Hot Springs](advent-of-code-2023/advent-of-code-2023/Solutions/Day_12.cs)**
 
 # Advent of Code 2022 :christmas_tree:
 
---
+++ advent-of-code-2023/advent-of-code-2023/Solutions/Day_12.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCode2022.Tools;
+using System.Text;
+
+namespace advent_of_code_2023.Solutions
+{
+    internal static class Day_12
+    {
+        public static void Solve()
+        {
+            AoCTools.RunMeasureTimeAndLog(Part1, Part2, "12", testInput: false);
+        }
+
+        private static int Part1(IEnumerable<string> input)
+        {
+            int totalArrangements = 0;
+
+            foreach (string str in input)
+            {
+                var data = str.Split(' ');
+
+                string initialLine = "." + data[0] + ".";
+                List<int> originalDamagedSprings = data[1].Split(',').Select(x => int.Parse(x)).ToList();
+                List<int> damagedSprings = data[1].Split(',').Select(x => int.Parse(x)).ToList();
+
+                HashSet<string> arrangements = new() { initialLine };
+
+                while(damagedSprings.Count > 0)
+                {
+                    HashSet<string> newArrangements = new();
+
+                    int currentDamaged = damagedSprings[0];
+                    damagedSprings.Remove(currentDamaged);
+
+                    foreach(var arr in arrangements)
+                    {
+                        for (int i = 0; i < initialLine.Length; i++)
+                        {
+                            StringBuilder lineCopy = new StringBuilder(arr);
+                            int remainingDamaged = currentDamaged;
+                            int j = i;
+
+                            if (lineCopy[j] == '.' || lineCopy[j - 1] == '#') continue;
+
+                            while (remainingDamaged > 0)
+                            {
+                                if (lineCopy[j] == '#' || lineCopy[j] == '?')
+                                {
+                                    lineCopy[j] = '#';
+                                    remainingDamaged--;
+                                    j++;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                            if (remainingDamaged == 0 && lineCopy[j] != '#')
+                            {
+                                newArrangements.Add(lineCopy.ToString());
+                            }
+                        }
+                    }
+
+                    arrangements.Clear();
+                    arrangements = newArrangements;
+                }
+
+                HashSet<string> validArrangements = new();
+
+                foreach (var arr in arrangements)
+                {
+                    var damagedStringsInNewArr = GetExistingDamagedStrings(arr);
+                    bool valid = true;
+
+                    for (int i = 0; i < damagedStringsInNewArr.Count; i++)
+                    {
+                        if (damagedStringsInNewArr.Count != originalDamagedSprings.Count ||
+                            damagedStringsInNewArr[i] != originalDamagedSprings[i]) valid = false;
+                    }
+
+                    if (valid) validArrangements.Add(arr);
+                }
+
+                totalArrangements += validArrangements.Count;
+            }
+
+            return totalArrangements;
+        }
+
+        private static int Part2(IEnumerable<string> input)
+        {
+            return 0;
+        }
+
+        private static List<int> GetExistingDamagedStrings(string line)
+        {
+            List<int> damagedStrings = new();
+
+            int start = -1;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '#' && start == -1) start = i;
+
+                if (line[i] != '#' && start != -1)
+                {
+                    damagedStrings.Add(i - start);
+                    start = -1;
+                }
+            }
+
+            return damagedStrings;
+        }
+    }
+}
