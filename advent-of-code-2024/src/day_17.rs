920b5467841640dfdc67eed9c2d1cb1109dce889
Jakub <kuba.kre@o2.pl>
2024-12-24 03:05:13 +0000

    Day_17 solution

--- advent-of-code-2024/src/day_17.rs
+++ advent-of-code-2024/src/day_17.rs
@@ -5,44 +5,50 @@ use crate::utilities::get_input_lines;
 const DAY_STRING : &str = "day_17";
 const USE_TEST_DATA : bool = false;
 
-fn perform_operation(instruction_code : char, operand : char) {
+struct State {
+    register_a: i64,
+    register_b: i64,
+    register_c: i64,
+    instruction_pointer : i64,
+    output : Vec<i64>
+}
+
+fn perform_operation(instruction_code : char, operand : char, state : &mut State) {
     match instruction_code {
-        '0' => adv(operand),
-        '1' => bxl(operand),
-        '2' => bst(operand),
-        '3' => jnx(operand),
-        '4' => bxc(operand),
-        '5' => out(operand),
-        '6' => bdv(operand),
-        '7' => cdv(operand),
+        '0' => adv(operand, state),
+        '1' => bxl(operand, state),
+        '2' => bst(operand, state),
+        '3' => jnx(operand, state),
+        '4' => bxc(operand, state),
+        '5' => out(operand, state),
+        '6' => bdv(operand, state),
+        '7' => cdv(operand, state),
         _ => {
             println!("THIS SHOULD NOT HAPPEN - INCORRECT INSTRUCTION CODE");
         }
     }
 }
 
-fn get_combo_operand_value(combo_operand : char) -> i64 {
-    unsafe {
-        let result = match combo_operand {
-            '0' => 0,
-            '1' => 1,
-            '2' => 2,
-            '3' => 3,
-            '4' => REGISTER_A,
-            '5' => REGISTER_B,
-            '6' => REGISTER_C,
-            '7' => {
-                println!("THIS SHOULD NOT HAPPEN - COMBO OPERAND 7 SHOULD NOT APPEAR IN VALID PROGRAMS");
-                return 0;
-            }
-            _ => {
-                println!("THIS SHOULD NOT HAPPEN - COMBO OPERAND IN DEFAULT CASE");
-                return 0;
-            }
-        };
+fn get_combo_operand_value(combo_operand : char, state : &State) -> i64 {
+    let result = match combo_operand {
+        '0' => 0,
+        '1' => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => state.register_a,
+        '5' => state.register_b,
+        '6' => state.register_c,
+        '7' => {
+            println!("THIS SHOULD NOT HAPPEN - COMBO OPERAND 7 SHOULD NOT APPEAR IN VALID PROGRAMS");
+            return 0;
+        }
+        _ => {
+            println!("THIS SHOULD NOT HAPPEN - COMBO OPERAND IN DEFAULT CASE");
+            return 0;
+        }
+    };
 
-        return result;
-    }
+    return result;
 }
 
 fn get_literal_operand_value(literal_operand : char) -> i64 {
@@ -54,153 +60,132 @@ fn get_literal_operand_value(literal_operand : char) -> i64 {
 // The denominator is found by raising 2 to the power of the instruction's combo operand.
 // (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
 // The result of the division operation is truncated to an integer and then written to the A register.
-fn adv(combo_operand : char) {
-    let combo_operand_value = get_combo_operand_value(combo_operand);
+fn adv(combo_operand : char, state : &mut State) {
+    let combo_operand_value = get_combo_operand_value(combo_operand, state);
 
     // Write result to A register
-    unsafe {
-        let denominator = 2_i64.pow(combo_operand_value as u32);
+    let denominator = 2_i64.pow(combo_operand_value as u32);
 
-        let result : i64 = REGISTER_A / denominator;
+    let result : i64 = state.register_a / denominator;
 
-        // println!("adv: {} / {} = {} -> saving in REGISTER_A", REGISTER_A, denominator, result);
-        REGISTER_A = result;
+    // println!("adv: {} / {} = {} -> saving in REGISTER_A", REGISTER_A, denominator, result);
+    state.register_a = result;
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
 
 // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's
 // literal operand, then stores the result in register B.
-fn bxl(literal_operand : char) {
+fn bxl(literal_operand : char, state : &mut State) {
     let literal_operand_value = get_literal_operand_value(literal_operand);
 
-    unsafe {
-        let result : i64 = REGISTER_B ^ literal_operand_value;
-        // println!("bxl: {} ^ {} = {} -> saving in REGISTER_B", REGISTER_B, literal_operand_value, result);
-        REGISTER_B = result; 
+    let result : i64 = state.register_b ^ literal_operand_value;
+    // println!("bxl: {} ^ {} = {} -> saving in REGISTER_B", REGISTER_B, literal_operand_value, result);
+    state.register_b = result; 
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
 
 // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby
 // keeping only its lowest 3 bits), then writes that value to the B register.
-fn bst(combo_operand : char) {
-    let combo_operand_value = get_combo_operand_value(combo_operand);
+fn bst(combo_operand : char, state : &mut State) {
+    let combo_operand_value = get_combo_operand_value(combo_operand, state);
     let result = combo_operand_value % 8;
 
-    unsafe {
-        // println!("bst: {} % 8 = {} -> saving in REGISTER_B", combo_operand_value, result);
-        REGISTER_B = result;
+    // println!("bst: {} % 8 = {} -> saving in REGISTER_B", combo_operand_value, result);
+    state.register_b = result;
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
 
 // The jnz instruction (opcode 3) does nothing if the A register is 0.
 // However, if the A register is not zero, it jumps by setting the instruction
 // pointer to the value of its literal operand; if this instruction jumps,
 // the instruction pointer is not increased by 2 after this instruction.
-fn jnx(literal_operand : char) {    
-    unsafe {
-        if REGISTER_A != 0 {
-            let literal_operand_value = get_literal_operand_value(literal_operand);
-            INSTRUCTION_POINTER = literal_operand_value;
-            // println!("jnx: setting INSTRUCTION_POINTER to {}", INSTRUCTION_POINTER);
-        } else {
-            // Apply instruction pointer offset
-            INSTRUCTION_POINTER += 2;
-            // println!("jnx: doing nothing");
-        }
+fn jnx(literal_operand : char, state : &mut State) {    
+    if state.register_a != 0 {
+        let literal_operand_value = get_literal_operand_value(literal_operand);
+        state.instruction_pointer = literal_operand_value;
+        // println!("jnx: setting INSTRUCTION_POINTER to {}", INSTRUCTION_POINTER);
+    } else {
+        // Apply instruction pointer offset
+        state.instruction_pointer += 2;
+        // println!("jnx: doing nothing");
     }
 }
 
 // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores
 // the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
-fn bxc(_ignored_operand : char) {
-    unsafe {
-        let result = REGISTER_B ^ REGISTER_C;
-        // println!("bst: {} ^ {} = {} -> saving in REGISTER_B", REGISTER_B, REGISTER_C, result);
-        REGISTER_B = result;
+fn bxc(_ignored_operand : char, state : &mut State) {
+    let result = state.register_b ^ state.register_c;
+    // println!("bst: {} ^ {} = {} -> saving in REGISTER_B", REGISTER_B, REGISTER_C, result);
+    state.register_b = result;
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
 
 // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that
 // value. (If a program outputs multiple values, they are separated by commas.)
-fn out(combo_operand : char) {
-    let combo_operand_value = get_combo_operand_value(combo_operand);
+fn out(combo_operand : char, state : &mut State) {
+    let combo_operand_value = get_combo_operand_value(combo_operand, state);
     let result = combo_operand_value % 8;
     // println!("out: {} % 8 = {} -> OUTPUT", combo_operand_value, result);
-    unsafe {
-        OUTPUT.push(result);
+    state.output.push(result);
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
 
 // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored
 // in the B register. (The numerator is still read from the A register.)
-fn bdv(combo_operand : char) {
-    let combo_operand_value = get_combo_operand_value(combo_operand);
+fn bdv(combo_operand : char, state : &mut State) {
+    let combo_operand_value = get_combo_operand_value(combo_operand, state);
 
     // Write result to A register
-    unsafe {
-        let denominator = 2_i64.pow(combo_operand_value as u32);
+    let denominator = 2_i64.pow(combo_operand_value as u32);
 
-        let result : i64 = REGISTER_A / denominator;
+    let result : i64 = state.register_a / denominator;
 
-        // println!("bdv: {} / {} = {} -> saving in REGISTER_B", REGISTER_B, denominator, result);
-        REGISTER_B = result;
+    // println!("bdv: {} / {} = {} -> saving in REGISTER_B", REGISTER_B, denominator, result);
+    state.register_b = result;
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
 
 // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored
 // in the C register. (The numerator is still read from the A register.)
-fn cdv(combo_operand : char) {
-    let combo_operand_value = get_combo_operand_value(combo_operand);
+fn cdv(combo_operand : char, state : &mut State) {
+    let combo_operand_value = get_combo_operand_value(combo_operand, state);
 
     // Write result to A register
-    unsafe {
-        let denominator = 2_i64.pow(combo_operand_value as u32);
+    let denominator = 2_i64.pow(combo_operand_value as u32);
 
-        let result : i64 = REGISTER_A / denominator;
+    let result : i64 = state.register_a / denominator;
 
-        // println!("cdv: {} / {} = {} -> saving in REGISTER_C", REGISTER_C, denominator, result);
-        REGISTER_C = result;
+    // println!("cdv: {} / {} = {} -> saving in REGISTER_C", REGISTER_C, denominator, result);
+    state.register_c = result;
 
-        // Apply instruction pointer offset
-        INSTRUCTION_POINTER += 2;
-    }
+    // Apply instruction pointer offset
+    state.instruction_pointer += 2;
 }
-
-static mut REGISTER_A : i64 = 0;
-static mut REGISTER_B : i64 = 0;
-static mut REGISTER_C : i64 = 0;
-
-static mut INSTRUCTION_POINTER : i64 = 0;
-static mut OUTPUT : Vec<i64> = vec![];
 
 #[allow(dead_code)]
 pub fn part_1() -> String
 {
     let input = get_input_lines(DAY_STRING, USE_TEST_DATA);
 
-    unsafe {
-        REGISTER_A = input[0].split_whitespace().last().unwrap().to_string().parse().expect("fail");
-        REGISTER_B = input[1].split_whitespace().last().unwrap().to_string().parse().expect("fail");
-        REGISTER_C = input[2].split_whitespace().last().unwrap().to_string().parse().expect("fail");
-    }
+    let mut state = State {
+        register_a: input[0].split_whitespace().last().unwrap().to_string().parse().expect("fail"),
+        register_b: input[1].split_whitespace().last().unwrap().to_string().parse().expect("fail"),
+        register_c: input[2].split_whitespace().last().unwrap().to_string().parse().expect("fail"),
+        instruction_pointer: 0,
+        output: vec![]
+    };
 
     let instructions_string : String = input[4].split_whitespace().last().unwrap().to_string();
 
@@ -208,45 +193,12 @@ pub fn part_1() -> String
                                   .filter_map(|s| s.chars().next()) // Take the first character of each part
                                   .collect();
 
-    // JUST TESTING
-    // unsafe {
-    //     adv('2');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-
-    //     bxl('2');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-
-    //     bst('4');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-
-    //     jnx('2');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-
-    //     REGISTER_C = 5;
-    //     bxc('0');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-
-    //     out('4');
-    //     out('5');
-    //     out('6');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-    //     println!("Output: {:?}", OUTPUT);
-
-    //     bdv('2');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-
-    //     cdv('2');
-    //     println!("InstructionPointer: {}, Registers: A={}, B={}, C={}", INSTRUCTION_POINTER, REGISTER_A, REGISTER_B, REGISTER_C);
-    // }
-
     let mut result : String;
 
-    unsafe {
-        while INSTRUCTION_POINTER < instructions.len() as i64 {
-            perform_operation(instructions[INSTRUCTION_POINTER as usize], instructions[(INSTRUCTION_POINTER + 1) as usize]);
-        }
-        result = OUTPUT.iter().map( |&id| id.to_string() + ",").collect(); 
+    while state.instruction_pointer < instructions.len() as i64 {
+        perform_operation(instructions[state.instruction_pointer as usize], instructions[(state.instruction_pointer + 1) as usize], &mut state);
     }
+    result = state.output.iter().map( |&id| id.to_string() + ",").collect(); 
     result.pop();
 
     return result.to_string();
@@ -262,7 +214,36 @@ pub fn part_2() -> String
 
     // Commenting out below code to avoid repeating the computation when running all solutions in main.rs
 
+    // Output of below code:
 
+    // #7032631133083: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 7032631133083
+    // #20986963100571: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
+    // #42217003221915: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #56171335189403: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
+    // #77401375310747: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #91355707278235: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
+    // #112585747399579: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #124323876242331: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 11738128842752
+    // #124341056111515: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 17179869184
+    // #126540079367067: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 2199023255552
+    // #147770119488411: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #161724451455899: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
+    // #182954491577243: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #196908823544731: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
+    // #218138863666075: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #232093195633563: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
+    // #253323235754907: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #265061364597659: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 11738128842752
+    // #265061364597659: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5, 3], diff: 0
+    // #265061364597659: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5, 3, 0], diff: 0         <----- FIRST OCCURENCE
+    // #265078544466843: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 17179869184
+    // #265078544466843: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5, 3], diff: 0
+    // #265078544466843: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5, 3, 0], diff: 0
+    // #267277567722395: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 2199023255552
+    // #267277567722395: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5, 3], diff: 0
+    // #267277567722395: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5, 3, 0], diff: 0
+    // #288507607843739: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 21230040121344
+    // #302461939811227: [2, 4, 1, 7, 7, 5, 1, 7, 4, 6, 0, 3, 5, 5], diff: 13954331967488
 
     // let input = get_input_lines(DAY_STRING, USE_TEST_DATA);
     // let instructions_string : String = input[4].split_whitespace().last().unwrap().to_string();
@@ -277,30 +258,37 @@ pub fn part_2() -> String
     // let mut prev : i64 = 0;
     // let mut counter : i64 = 62587803;
 
+    // let mut state = State {
+    //     register_a: counter,
+    //     register_b: 0,
+    //     register_c: 0,
+    //     instruction_pointer: 0,
+    //     output: vec![]
+    // };
+
     // while counter < 500000000000000000 {
     //     counter += 4194304;
-    //     unsafe {
-    //         INSTRUCTION_POINTER = 0;
-    //         REGISTER_A = counter;
-    //         REGISTER_B = 0;
-    //         REGISTER_C = 0;
-    //         OUTPUT.clear();
 
-    //         let mut output_len = 0;
-    //         while INSTRUCTION_POINTER < instructions.len() as i64 {
-    //             perform_operation(instructions[INSTRUCTION_POINTER as usize], instructions[(INSTRUCTION_POINTER + 1) as usize]);
-            
-    //             if output_len < OUTPUT.len() {
-    //                 if instructions_i64[output_len] != OUTPUT[output_len] {
-    //                     break;
-    //                 } else {
-    //                     if OUTPUT.len() >= 14 {
-    //                         println!("#{}: {:?}, diff: {}", counter, OUTPUT, counter - prev);
-    //                         prev = counter;
-    //                     }
+    //     state.register_a = counter;
+    //     state.register_b = 0;
+    //     state.register_c = 0;
+    //     state.instruction_pointer = 0;
+    //     state.output.clear();
+
+    //     let mut output_len = 0;
+    //     while state.instruction_pointer < instructions.len() as i64 {
+    //         perform_operation(instructions[state.instruction_pointer as usize], instructions[(state.instruction_pointer + 1) as usize], &mut state);
+        
+    //         if output_len < state.output.len() {
+    //             if instructions_i64[output_len] != state.output[output_len] {
+    //                 break;
+    //             } else {
+    //                 if state.output.len() >= 14 {
+    //                     println!("#{}: {:?}, diff: {}", counter, state.output, counter - prev);
+    //                     prev = counter;
     //                 }
-    //                 output_len = OUTPUT.len();
     //             }
+    //             output_len = state.output.len();
     //         }
     //     }
     // }
