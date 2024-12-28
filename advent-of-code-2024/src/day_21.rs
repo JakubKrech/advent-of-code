acd32fcb02200218bb647d0cca8ce874f1c0f424
Jakub <kuba.kre@o2.pl>
2024-12-28 01:37:57 +0000

    Day_21 part 2

--- advent-of-code-2024/src/day_21.rs
+++ advent-of-code-2024/src/day_21.rs
@@ -1,12 +1,125 @@
 use crate::utilities::get_input_lines;
-use std::{collections::HashMap, usize};
+use std::{collections::HashMap, u64, usize};
 
 // https://adventofcode.com/2024/day/21
 
 const DAY_STRING : &str = "day_21";
 const USE_TEST_DATA : bool = false;
 
-fn press_directional_keyboard(current_arm_position : char, button_to_press : char) -> (char, String) {
+#[allow(dead_code)]
+pub fn part_1() -> String {
+    let lowest_sum_of_complexities : u64 = find_lowest_sum_of_complexities(&get_input_lines(DAY_STRING, USE_TEST_DATA), 2);
+    return lowest_sum_of_complexities.to_string();
+}
+
+pub fn part_2() -> String {
+    let lowest_sum_of_complexities : u64 = find_lowest_sum_of_complexities(&get_input_lines(DAY_STRING, USE_TEST_DATA), 25);
+    return lowest_sum_of_complexities.to_string();
+}
+
+pub fn find_lowest_sum_of_complexities(input_codes : &Vec<String>, directional_keypad_robot_count : usize) -> u64 {
+    let mut lowest_sum_of_complexities : u64 = u64::MAX;
+
+    // 4 out of 5 directional keypad buttons have two possible robot paths to click some buttons.
+    // All of available path options need to be tested to find optimal solution.
+    for option_a in 0..2 {
+        for option_b in 0..2 {
+            for option_c in 0..2 {
+                for option_d in 0..2 {
+                    let score = get_sum_of_complexities(input_codes, option_a, option_b, option_c, option_d, directional_keypad_robot_count);
+                    if lowest_sum_of_complexities > score {
+                        lowest_sum_of_complexities = score;
+                    }
+                }
+            }
+        }
+    }
+
+    return lowest_sum_of_complexities
+}
+
+pub fn get_sum_of_complexities(input_codes : &Vec<String>, opt_a : usize, opt_b : usize, opt_c : usize, opt_d : usize, directional_keypad_robot_count : usize) -> u64
+{
+    let mut sum_of_complexities : u64 = 0;
+
+    for line in input_codes {
+        // Phase I - translate code (ex: 029A) to moves on a numeric keypad. There might be multiple combinations as there are many button-button paths.
+        let (_, directional_keypad_moves) = translate_numeric_keypad_into_directional_keypad_moves(line.clone());
+
+        // Phase II - translate numeric keypad moves to directional keypad moves
+        let mut directional_keypad_robot_arm_movements : Vec<HashMap<String, usize>> = vec![];
+
+        // Phase III - start using HashSet to count how many types of movement will need to be done by each robot to optimize further computation
+        for rdm in &directional_keypad_moves {
+            let mut robot_arm_movements_count : HashMap<String, usize> = HashMap::new();
+            let complete_robot_arm_movements = separate_list_of_moves_to_complete_movements_vector(rdm.to_string());
+            for m in complete_robot_arm_movements {
+                *robot_arm_movements_count.entry(m).or_insert(0) += 1;
+            }
+            directional_keypad_robot_arm_movements.push(robot_arm_movements_count);
+        }
+
+        // Phase IV - each robot will need to make more moves, to operate the previous robot's arm. Process the robot chain.
+        for _ in 0..directional_keypad_robot_count {
+
+            let mut new_directional_keypad_robot_arm_movements : Vec<HashMap<String, usize>> = vec![];
+
+            for dkram in &directional_keypad_robot_arm_movements {
+                let processed = process_robot_to_robot(dkram.clone(), opt_a, opt_b, opt_c, opt_d);
+
+                for p in &processed {
+                    new_directional_keypad_robot_arm_movements.push(p.clone());
+                }
+            }
+
+            directional_keypad_robot_arm_movements = std::mem::take(&mut new_directional_keypad_robot_arm_movements);
+        }
+
+        // Phase V - Find lowest number of keypad button clicks that human needs to make
+        let mut lowest_keypad_button_clicks : usize = usize::MAX;
+        for x in &directional_keypad_robot_arm_movements {
+            let mut keypad_button_clicks : usize = 0;
+            for entry in x {
+                keypad_button_clicks += entry.0.len() * entry.1;
+            }
+            if lowest_keypad_button_clicks > keypad_button_clicks {
+                lowest_keypad_button_clicks = keypad_button_clicks;
+            }
+        }
+
+        // Phase VI - Translate codes to u64 numbers and calculate the complexity
+        let mut code_digits = line.clone();
+        code_digits.pop().expect("Failed to pop").to_string();
+        let code_as_num : u64 = code_digits.parse().expect("Failed to parse");
+
+        sum_of_complexities += code_as_num * lowest_keypad_button_clicks as u64;
+    }
+
+    return sum_of_complexities;
+}
+
+fn process_robot_to_robot(move_strings : HashMap<String, usize>, a : usize, b : usize, c : usize, d : usize) -> Vec<HashMap<String, usize>> {
+    let mut required_moves : Vec<HashMap<String, usize>> = vec![];
+    required_moves.push(HashMap::new());
+
+    for move_string in &move_strings {
+        let multiplier = move_string.1;
+        let mut curr_position = 'A';
+
+        for move_char in move_string.0.chars() {
+            let req_moves = press_directional_keyboard(curr_position, move_char, a, b, c, d);
+            curr_position = req_moves.0;
+
+            for rm in &mut required_moves {
+                *rm.entry(format!("{}A", req_moves.1.clone())).or_insert(0) += 1 * multiplier;
+            }
+        }
+    }
+
+    return required_moves;
+}
+
+fn press_directional_keyboard(current_arm_position : char, button_to_press : char, opt_a : usize, opt_b : usize, opt_c : usize, opt_d : usize) -> (char, String) {
 
     if current_arm_position == button_to_press {
         return (button_to_press, "".to_string());
@@ -14,17 +127,26 @@ fn press_directional_keyboard(current_arm_position : char, button_to_press : cha
 
     let mut directional_keyboard_schemes : HashMap<(char, char), String> = HashMap::new();
 
+    // Some button-button moves have more than one path that can be reached with optimal ammount of robot arm moves.
+    // All need to be tested to find out which combination is most optimal.
+    let mut optional_schemes_a : Vec<((char, char), String)> = vec![];
+    let mut optional_schemes_up : Vec<((char, char), String)> = vec![];
+    let mut optional_schemes_down : Vec<((char, char), String)> = vec![];
+    let mut optional_schemes_right : Vec<((char, char), String)> = vec![];
+
     // current == A
     directional_keyboard_schemes.insert(('A', '^'), "<".to_string());
     directional_keyboard_schemes.insert(('A', '<'), "v<<".to_string());
-    directional_keyboard_schemes.insert(('A', 'v'), "<v,v<".to_string());
     directional_keyboard_schemes.insert(('A', '>'), "v".to_string());
+    optional_schemes_a.push((('A', 'v'), "<v".to_string()));
+    optional_schemes_a.push((('A', 'v'), "v<".to_string()));
 
     // current == ^
     directional_keyboard_schemes.insert(('^', 'A'), ">".to_string());
     directional_keyboard_schemes.insert(('^', '<'), "v<".to_string());
-    directional_keyboard_schemes.insert(('^', 'v'), "<".to_string());
-    directional_keyboard_schemes.insert(('^', '>'), "v>".to_string());
+    directional_keyboard_schemes.insert(('^', 'v'), "v".to_string());
+    optional_schemes_up.push((('^', '>'), ">v".to_string()));
+    optional_schemes_up.push((('^', '>'), "v>".to_string()));
 
     // current == <
     directional_keyboard_schemes.insert(('<', 'A'), ">>^".to_string());
@@ -33,16 +155,30 @@ fn press_directional_keyboard(current_arm_position : char, button_to_press : cha
     directional_keyboard_schemes.insert(('<', '>'), ">>".to_string());
 
     // current == v
-    directional_keyboard_schemes.insert(('v', 'A'), ">^,^>".to_string());
     directional_keyboard_schemes.insert(('v', '^'), "^".to_string());
     directional_keyboard_schemes.insert(('v', '<'), "<".to_string());
     directional_keyboard_schemes.insert(('v', '>'), ">".to_string());
+    optional_schemes_down.push((('v', 'A'), ">^".to_string()));
+    optional_schemes_down.push((('v', 'A'), "^>".to_string()));
 
     // current == >
     directional_keyboard_schemes.insert(('>', 'A'), "^".to_string());
-    directional_keyboard_schemes.insert(('>', '^'), "^<,<^".to_string());
     directional_keyboard_schemes.insert(('>', '<'), "<<".to_string());
     directional_keyboard_schemes.insert(('>', 'v'), "<".to_string());
+    optional_schemes_right.push((('>', '^'), "<^".to_string()));
+    optional_schemes_right.push((('>', '^'), "^<".to_string()));
+
+    let aaa = &optional_schemes_a[opt_a];
+    directional_keyboard_schemes.insert(aaa.0, aaa.1.clone());
+
+    let bbb = &optional_schemes_up[opt_b];
+    directional_keyboard_schemes.insert(bbb.0, bbb.1.clone());
+
+    let ccc = &optional_schemes_down[opt_c];
+    directional_keyboard_schemes.insert(ccc.0, ccc.1.clone());
+
+    let ddd = &optional_schemes_right[opt_d];
+    directional_keyboard_schemes.insert(ddd.0, ddd.1.clone());
 
     return (button_to_press, directional_keyboard_schemes[&(current_arm_position, button_to_press)].clone());
 }
@@ -190,19 +326,34 @@ fn press_numeric_keyboard(current_arm_position : char, button_to_press : char) -
     return (button_to_press, numeric_keyboard_schemes[&(current_arm_position, button_to_press)].clone());
 }
 
-fn translate_code_into_numeric_moves(code : String, cur_num_pos : char) -> (char, Vec<String>) {
+fn separate_list_of_moves_to_complete_movements_vector(moves_string : String) -> Vec<String> {
+    
+    let mut complete_robot_arm_movements : Vec<String> = vec![];
+    let mut start_index = 0;
+    
+    for (end_index, ch) in moves_string.char_indices() {
+        if ch == 'A' {
+            // Collect the substring up to the 'A'
+            complete_robot_arm_movements.push(moves_string[start_index..=end_index].to_string());
+            start_index = end_index + 1;
+        }
+    }
+    return complete_robot_arm_movements;
+}
 
-    let mut cur_num_position: char = cur_num_pos;
+fn translate_numeric_keypad_into_directional_keypad_moves(code : String) -> (char, Vec<String>) {
+
+    let mut cur_num_position: char = 'A';
     let mut required_moves : Vec<String> = vec!["".to_string()];
 
     for c in code.chars() {
         let (new_position, s) = press_numeric_keyboard(cur_num_position, c);
-
-        let new_possible_moves : Vec<&str> = s.split_terminator(',').collect();
-
-        // println!("Moved from {} to {}, path: {:?}", cur_num_position, new_position, new_possible_moves);
-
         cur_num_position = new_position;
+        let mut new_possible_moves : Vec<&str> = s.split_terminator(',').collect();
+
+        if s == "" {
+            new_possible_moves.push("");
+        }
 
         let mut new_required_moves : Vec<String> = vec![];
 
@@ -214,156 +365,7 @@ fn translate_code_into_numeric_moves(code : String, cur_num_pos : char) -> (char
 
         required_moves.clear();
         required_moves = std::mem::take(&mut new_required_moves);
-
-        // println!("{:?}", required_moves);
     }
 
     return (cur_num_position, required_moves);
-}
-
-fn translate_num_moves_into_dir_moves(num_moves : Vec<String>, cur_dir_pos : char) -> (char, Vec<String>) {
-
-    let mut cur_dir_position: char = cur_dir_pos;
-    let mut all_required_dir_moves : Vec<String> = vec![];
-
-    for num_moves in &num_moves {
-
-        // let mut num_moves_with_a_click = "".to_string();
-
-        // for nm in num_moves.chars() {
-        //     num_moves_with_a_click.push_str(&format!("{}A", nm));
-        // }
-
-        let mut required_dir_moves : Vec<String> = vec!["".to_string()];
-
-        for m in num_moves.chars() {
-            let (new_position, s) = press_directional_keyboard(cur_dir_position, m);
-            cur_dir_position = new_position;
-
-            // if s == "" {
-            //     continue;
-            // }
-
-            let new_possible_moves : Vec<&str> = s.split_terminator(',').collect();
-
-            // println!("New possible moves for {}: {:?}", m, new_possible_moves);
-
-            let mut new_required_moves : Vec<String> = vec![];
-
-            if s == "" {
-                for rm in &required_dir_moves {
-                    new_required_moves.push(format!("{}A", rm));
-                }
-            } else {
-                for npm in &new_possible_moves {
-                    for rm in &required_dir_moves {
-                        new_required_moves.push(format!("{}{}A", rm, npm));
-                    }
-                }
-            }
-
-            required_dir_moves.clear();
-            required_dir_moves = std::mem::take(&mut new_required_moves);
-
-            // println!("{:?}", required_dir_moves);
-        }
-
-        for x in required_dir_moves {
-            all_required_dir_moves.push(x);
-        }
-    }
-
-    // println!("Num -> Dir moves: '{:?}' -> '{:?}'", num_moves, all_required_dir_moves);
-
-    return (cur_dir_position, all_required_dir_moves);
-}
-
-#[allow(dead_code)]
-pub fn part_1() -> String
-{
-    let input = get_input_lines(DAY_STRING, USE_TEST_DATA);
-
-    let mut result : usize = 0;
-
-    for line in input {
-
-        let cur_num_position: char = 'A';
-        let cur_dir_position: char = 'A';
-
-        // Phase I - translate code (ex: 029A) to moves on a numeric keypad
-        let (_, required_num_moves) = translate_code_into_numeric_moves(line.clone(), cur_num_position);
-
-        // Phase II - translate numeric keypad moves to directional keypad moves
-        let mut required_moves = required_num_moves.clone();
-
-        for i in 0..2 {
-            println!("#{}: {}", i, required_moves.len());
-            let (_, mut required_dir_moves) = translate_num_moves_into_dir_moves(required_moves.clone(), cur_dir_position);
-            required_moves.clear();
-            required_moves = std::mem::take(&mut required_dir_moves)
-        }
-
-        let mut shortest : usize = usize::MAX;
-
-        for x in required_moves {
-            if x.len() < shortest {
-                shortest = x.len();
-            }
-        }
-
-        let mut code_digits = line.clone();
-        code_digits.pop().expect("Failed to pop").to_string();
-        let code_as_num : usize = code_digits.parse().expect("Failed to parse");
-
-        result += code_as_num * shortest;
-
-        println!("Shortest combination length for code {}: {}. Digits: {}, value: {}", line, shortest, code_digits, code_as_num * shortest);
-    }
-
-    return result.to_string();
-}
-
-#[allow(dead_code)]
-pub fn part_2() -> String
-{
-    let input = get_input_lines(DAY_STRING, USE_TEST_DATA);
-
-    let mut result : usize = 0;
-
-    for line in input {
-
-        let cur_num_position: char = 'A';
-        let cur_dir_position: char = 'A';
-
-        // Phase I - translate code (ex: 029A) to moves on a numeric keypad
-        let (_, required_num_moves) = translate_code_into_numeric_moves(line.clone(), cur_num_position);
-
-        // Phase II - translate numeric keypad moves to directional keypad moves
-        let mut required_moves = required_num_moves.clone();
-
-        for i in 0..3 {
-            println!("#{}: {}", i, required_moves.len());
-            let (_, mut required_dir_moves) = translate_num_moves_into_dir_moves(required_moves.clone(), cur_dir_position);
-            required_moves.clear();
-            required_moves = std::mem::take(&mut required_dir_moves)
-        }
-
-        let mut shortest : usize = usize::MAX;
-
-        for x in &required_moves {
-            if x.len() < shortest {
-                shortest = x.len();
-            }
-        }
-
-        let mut code_digits = line.clone();
-        code_digits.pop().expect("Failed to pop").to_string();
-        let code_as_num : usize = code_digits.parse().expect("Failed to parse");
-
-        result += code_as_num * shortest;
-
-        println!("Shortest combination length for code {}: {}. Digits: {}, value: {}", line, shortest, code_digits, code_as_num * shortest);
-    }
-
-    return result.to_string();
 }
--- advent-of-code-2024/src/main.rs
+++ advent-of-code-2024/src/main.rs
@@ -5,27 +5,26 @@ mod day_01; mod day_02; mod day_03; mod day_04; mod day_05;
 mod day_06; mod day_07; mod day_08; mod day_09; mod day_10;
 mod day_11; mod day_12; mod day_13; mod day_14; mod day_15;
 mod day_16; mod day_17; mod day_18; mod day_19; mod day_20;
-// mod day_21;
-mod day_22; mod day_23; mod day_24; mod day_25;
+mod day_21; mod day_22; mod day_23; mod day_24; mod day_25;
 
 fn main() {
-    // run_solution("01", &day_01::part_1, &day_01::part_2);
-    // run_solution("02", &day_02::part_1, &day_02::part_2);
-    // run_solution("03", &day_03::part_1, &day_03::part_2);
-    // run_solution("04", &day_04::part_1, &day_04::part_2);
-    // run_solution("05", &day_05::part_1, &day_05::part_2);
+    run_solution("01", &day_01::part_1, &day_01::part_2);
+    run_solution("02", &day_02::part_1, &day_02::part_2);
+    run_solution("03", &day_03::part_1, &day_03::part_2);
+    run_solution("04", &day_04::part_1, &day_04::part_2);
+    run_solution("05", &day_05::part_1, &day_05::part_2);
  
-    // run_solution("06", &day_06::part_1, &day_06::part_2);
-    // run_solution("07", &day_07::part_1, &day_07::part_2);
-    // run_solution("08", &day_08::part_1, &day_08::part_2);
-    // run_solution("09", &day_09::part_1, &day_09::part_2);
-    // run_solution("10", &day_10::part_1, &day_10::part_2);
+    run_solution("06", &day_06::part_1, &day_06::part_2);
+    run_solution("07", &day_07::part_1, &day_07::part_2);
+    run_solution("08", &day_08::part_1, &day_08::part_2);
+    run_solution("09", &day_09::part_1, &day_09::part_2);
+    run_solution("10", &day_10::part_1, &day_10::part_2);
  
-    // run_solution("11", &day_11::part_1, &day_11::part_2);
-    // run_solution("12", &day_12::part_1, &day_12::part_2);
-    // run_solution("13", &day_13::part_1, &day_13::part_2);
-    // run_solution("14", &day_14::part_1, &day_14::part_2);
-    // run_solution("15", &day_15::part_1, &day_15::part_2);
+    run_solution("11", &day_11::part_1, &day_11::part_2);
+    run_solution("12", &day_12::part_1, &day_12::part_2);
+    run_solution("13", &day_13::part_1, &day_13::part_2);
+    run_solution("14", &day_14::part_1, &day_14::part_2);
+    run_solution("15", &day_15::part_1, &day_15::part_2);
     
     run_solution("16", &day_16::part_1, &day_16::part_2);
     run_solution("17", &day_17::part_1, &day_17::part_2);
@@ -33,9 +32,16 @@ fn main() {
     run_solution("19", &day_19::part_1, &day_19::part_2);    
     run_solution("20", &day_20::part_1, &day_20::part_2);
 
-    // run_solution("21", &day_21::part_1, &day_21::part_2);
+    run_solution("21", &day_21::part_1, &day_21::part_2);
     run_solution("22", &day_22::part_1, &day_22::part_2);
     run_solution("23", &day_23::part_1, &day_23::part_2);
-    run_solution("24", &day_24::part_1, &day_24::part_2);
+
+    // day 24 idea:
+    // calculate how many bits match -> try find swap of 2 outputs that will make one more bit match
+    // then find another swap of 2 that will fix another bit
+    // repeat two more times -> 4 pairs of outputs will be found, maybe it will work
+    run_solution("24", &day_24::part_1, &day_24::part_2); 
+    
+    
     run_solution("25", &day_25::part_1, &day_25::part_2);
 }
